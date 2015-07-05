9f3fbcc12c5fae0628920df0ac11426d1cbd1161
James Pike <jpike@chilon.net>
2015-07-05 11:57:37 +0100

    catches signals

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
+#define _XOPEN_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include <signal.h>
 
 #define MAX_CMDS 16
 #define SEP      "---"
+
+// used as boolean by signal handlers to tell process it should exit
+static int running = 1;
 
 static void wait_for_exit(int n_cmds, pid_t *pids) {
   int cmds_left = n_cmds;
@@ -15,10 +21,19 @@ static void wait_for_exit(int n_cmds, pid_t *pids) {
     int status;
     pid_t waited_pid = waitpid(-1, &status, 0);
     if (waited_pid == -1) {
-#     ifndef NDEBUG
-      fprintf(stderr, "waitpid returned -1\n");
-#     endif
-      return;
+      if (errno == EINTR) {
+#       ifndef NDEBUG
+        fprintf(stderr, "waitpid interrupted by signal\n");
+#       endif
+
+        if (! running)
+          return;
+      }
+      else {
+#       ifndef NDEBUG
+        fprintf(stderr, "waitpid returned unhandled error state\n");
+#       endif
+      }
     }
 
     // check for pid in list of child pids
@@ -28,8 +43,12 @@ static void wait_for_exit(int n_cmds, pid_t *pids) {
         fprintf(stderr, "process exit: %d in command list, %d left\n", waited_pid, cmds_left - 1);
 #       endif
 
-        if (--cmds_left == 0)
+        if (--cmds_left == 0) {
+#         ifndef NDEBUG
+          fprintf(stderr, "all processes exited\n");
+#         endif
           return;
+        }
         break;
       }
 #     ifndef NDEBUG
@@ -51,11 +70,38 @@ static pid_t run_proc(char **argv) {
   exit(1);
 }
 
+static void on_signal(int signum) {
+# ifndef NDEBUG
+  // probably not safe... at least it's only in debug mode ;)
+  fprintf(stderr, "got signal %d\n", signum);
+#endif
+  running = 0;
+}
+
+static void perror_die(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+static void install_signal_handlers() {
+  struct sigaction sa;
+  sa.sa_handler = on_signal;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(SIGINT, &sa, NULL))
+    perror_die("could not catch SIGINT");
+
+  if (sigaction(SIGTERM, &sa, NULL))
+    perror_die("could not catch SIGTERM");
+}
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     fprintf(stderr, "please supply at least one command to run\n");
     return 1;
   }
+
+  install_signal_handlers();
 
   pid_t cmds[MAX_CMDS];
   int n_cmds = 0;
@@ -77,9 +123,6 @@ int main(int argc, char *argv[]) {
   }
 
   wait_for_exit(n_cmds, cmds);
-# ifndef NDEBUG
-  fprintf(stderr, "all processes exited\n");
-# endif
 
   return 0;
 }
